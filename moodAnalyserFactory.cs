5a8692e2e17c79f9d660d075c5e8b38a4e5d19a3
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-21 13:31:14 +0530

    usecase 4:

---
+++ moodAnalyserFactory.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+
+
+namespace moodAnalyaserTest
+{
+    public class analyseMoodFactory<Gtype>
+    {
+        Type type = typeof(Gtype);
+        public ConstructorInfo defaultConstructor(int num=0)
+        {
+            try
+            {
+                Type type = typeof(Gtype);
+                ConstructorInfo[] constructors = type.GetConstructors();
+                foreach (var info in constructors)
+                {
+                    if (info.GetParameters().Length == num)
+                        return info;
+                }
+
+                return constructors[0];
+            }
+
+            catch (Exception)
+            {
+                throw new analyseMoodException(analyseMoodException.Exception_Type.No_Such_Method_Exception, "please enter proper mood");
+            }
+        }
+
+
+        public object GetInstance(string classname,ConstructorInfo constructor)
+
+        {
+            try
+            {
+                
+                if (classname != type.Name)
+                    throw new analyseMoodException(analyseMoodException.Exception_Type.No_Such_Class_Exception, "please enter right mood");
+                if (constructor != type.GetConstructors()[0])
+                    throw new analyseMoodException(analyseMoodException.Exception_Type.No_Such_Method_Exception, "please enter right mood");
+                var Object_return = constructor.Invoke(new object[0]);
+                Gtype Obj_retuen = Activator.CreateInstance<Gtype>();
+                return Obj_retuen;
+            }
+            catch (Exception)
+            {
+                throw new analyseMoodException(analyseMoodException.Exception_Type.Error_in_obj_ceation, "please enter right mood");
+            }
+        }
+
+    }
+}
