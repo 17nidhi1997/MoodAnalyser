f92858b85bce23f165c667454095c9fc02b1fee3
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-21 13:09:50 +0530

    usecase:4

--- moodAnalyser.cs
+++ moodAnalyser.cs
@@ -1,38 +1,51 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
-
+using System.Text
 namespace moodAnalyaserTest
 {
-    class analyseMood
+    class analyseMood3
     {
-        private string message;
+
+        public string message;
+        public analyseMood()
+        {
+            this.message = "i am in sad mood";
+        }
         public analyseMood(String message)
         {
             this.message = message;
         }
 
+        
         public String analyseMoodMethod(String message)
         {
             this.message = message;
             return analyseMoodMethod();
         }
 
-        public string analyseMoodMethod()
+        public String analyseMoodMethod()
         {
             try
             {
+                if ((message == null) || (message.Length == 0))
+                {
+                    throw new analyseMoodException(analyseMoodException.Exception_Type.Empty_Exception, "please enter proper mood");
+                   
+                }
                 if (message.Contains("sad"))
                     return "SAD";
-                else
+                else if (message.Contains("happy"))
                     return "HAPPY";
+                else throw new analyseMoodException(analyseMoodException.Exception_Type.Invalid_Exception, "please enter proper mood");
+              
             }
-            catch(NullReferenceException)
+            catch (NullReferenceException)
             {
-                return "HAPPY";
+
+                throw new analyseMoodException(analyseMoodException.Exception_Type.Null_Exception, "please enter proper mood");
             }
+
         }
 
     }
 }
-
