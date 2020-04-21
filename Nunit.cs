9f378e24aec0e098ffa0533e9dbe75ef954559fd
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-04-21 12:36:10 +0530

    usecase3:

--- Nunit.cs
+++ Nunit.cs
@@ -16,5 +16,49 @@ namespace moodAnalyaserTest
             Assert.AreEqual("HAPPY",mood.analyseMoodMethod());
 
         }
-    }
+
+        [Test]
+        public void invalidInput()
+        {
+            analyseMood mood = new analyseMood("aaaaaaaa");
+            Assert.AreEqual("HAPPY", mood.analyseMoodMethod());
+        }
+
+        [Test]
+        public void NullInput()
+        {
+            analyseMood mood = new analyseMood(null);
+            Assert.AreEqual("HAPPY", mood.analyseMoodMethod());
+        }
+        
+        [Test]
+       public void EmptyException()
+        {
+            try
+            {
+                analyseMood mood = new analyseMood("");
+                Assert.AreEqual("please enter proper mood", mood.analyseMoodMethod());
+            }
+            catch (analyseMoodException e)
+            {
+                Assert.AreEqual(analyseMoodException.Exception_Type.Empty_Exception, e.ex_ty);
+            }
+        }
+
+        [Test]
+        public void NullException()
+        {
+            try
+            {
+                analyseMood mood = new analyseMood(null);
+
+                Assert.AreEqual("please enter proper mood", mood.analyseMoodMethod());
+            }
+            catch (analyseMoodException e)
+            {
+                Assert.AreEqual(analyseMoodException.Exception_Type.Null_Exception, e.ex_ty);
+            }
+        }
+
+   }
 }
